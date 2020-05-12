c0591f3595114d2cb94cc0d31e745c11526c8a17
jgrimm3 <jgrimm@wpi.edu>
2020-05-12 17:17:09 -0400

    added recommenter to ndcg, pulled top 5 results. made 12 rubrics and added text file output of scoring

--- createRubric.py
+++ createRubric.py
@@ -2,14 +2,13 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 from youtube_api import YoutubeDataApi
-from youtube_api import youtube_api_utils
 import youtube_api.parsers as parser
-from sklearn.metrics import ndcg_score
 from openpyxl import Workbook
-from openpyxl import load_workbook
-import numpy as np
+import json
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#FILL IN API KEY BELOW
 youtube = YoutubeDataApi('AIzaSyBL9Nzzvnwl_xPfXPKOCFTADEuHm70iH74')
 youtubeRecommended = []
 recommenterRecommended = []
@@ -17,39 +16,61 @@ recommenterRecommended = []
 
 #Pull top K videos from youtube API
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def youtubeRecommender(ID, list):
-    videoInfo = youtube.get_recommended_videos( video_id= ID, max_results=10, parser= parser.raw_json )
+def youtubeRecommender(ID, listy):
+    videoInfo = youtube.get_recommended_videos( video_id= ID, max_results=5, parser= parser.raw_json )
     for dict in videoInfo:
         for id, value in dict["id"].items():
            if id == 'videoId':
-               list.append(value)
-    return list
+               listy.append(value)
+    return listy
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #Pull Top K videos from Recommenter
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def recommenterRecommender(ID, list):
+def recommenterRecommender(ID, listr):
+    vids = []
+    scores = []
+    comment_weight = 0
+    trans_weight = 0
+    with open("recommenter_related_0.3.json") as json_file:
+        rec_data= json.load(json_file)
+        results = rec_data[ID]
+        for key, value in results.items():
+            vids.append(key)
+            for feature, weight in value.items():
+                if feature == "comment":
+                    comment_weight = weight
+                elif feature == "transcript":
+                    trans_weight = weight
+
+            totalWeight = trans_weight + comment_weight
+            scores.append(totalWeight)
+
+        rec_total = zip(scores, vids)
+        sorted_rec = sorted(rec_total, key=lambda x: x[0], reverse=True)
+        true_Vids = [list(s) for s in zip(*sorted_rec)][1][:5]
 
 
-    return list
+
+    return true_Vids
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #Fill Excel Sheet
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def fill_Excell(youRanks, recRanks):
-    filename = "NDCG_Rubric.xlsx"
+def fill_Excell(inputURL, youRanks, recRanks):
+    filename = "NDCG_Rubric12.xlsx"
     workbook = Workbook()
     sheet = workbook.active
-    sheet.column_dimensions['A'].width = 50
-    sheet.cell(row=1, column=1).value = "Video Link"
+    sheet.column_dimensions['A'].width = 60
+    sheet.cell(row=1, column=1).value = "Comparison Video =  " + inputURL
     sheet.cell(row=1, column=2).value = "Rank 1 - 5"
-    for row in range(2, 22):
-        if row <= 11:
+    for row in range(2, 12):
+        if row <= 6:
             sheet.cell(row=row, column=1).hyperlink = "https://www.youtube.com/watch?v=" + youRanks[row - 2]
             sheet.cell(row=row, column=1).style = "Hyperlink"
         else:
-            ind = row - 11
+            ind = row - 6
             sheet.cell(row=row, column=1).hyperlink = "https://www.youtube.com/watch?v=" + recRanks[ind-1]
             sheet.cell(row=row, column=1).style = "Hyperlink"
     workbook.save(filename=filename)
@@ -59,8 +80,8 @@ def mainInput(inputURL):
 #have to run first once to populate excel, then comment out until the input line to pull and uncomment below input line
     Video_ID = inputURL.partition('v=')[2]
     youtubeRecommended = youtubeRecommender(Video_ID, [])
-    recommenterRecommended = youtubeRecommender(Video_ID, [])
-    fill_Excell(youtubeRecommended, recommenterRecommended)
+    recommenterRecommended = recommenterRecommender(Video_ID, [])
+    fill_Excell(inputURL, youtubeRecommended, recommenterRecommended)
 
 if __name__ == "__main__":
-    mainInput('https://www.youtube.com/watch?v=zuGkNHnjo8s')
+    mainInput('https://www.youtube.com/watch?v=No8-mBek3rs')
--- ndcg.py
+++ ndcg.py
@@ -12,7 +12,7 @@ import numpy as np
 #Pull Excel Sheet
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def pull_excelRanks():
-    wb = load_workbook("NDCG_Rubric.xlsx")
+    wb = load_workbook("NDCG_Rubric2.xlsx")
     sheet = wb.active
     true_scores = []
     true_vids = []
@@ -20,8 +20,8 @@ def pull_excelRanks():
     you_vids = []
     rec_scores = []
     rec_vids = []
-    for row in range(2, 22):
-        if row <= 11:
+    for row in range(2, 12):
+        if row <= 6:
             true_scores.append(int(sheet.cell(row=row, column=2).value))
             true_vids.append(str(sheet.cell(row=row, column=1).value))
             you_scores.append(int(sheet.cell(row=row, column=2).value))
@@ -41,38 +41,45 @@ def pull_excelRanks():
 
 #Eval NDCG
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def calc_NDCG(true_scores, rec_scores, you_scores):
+def calc_NDCG(true_vids, you_vids, rec_vids, true_scores, rec_scores, you_scores):
 
     true_relevance = np.asarray([true_scores])
-    youtube_NDCG = ndcg_score(true_relevance, np.asarray([you_scores]), k=10)
-    recommenter_NDCG = ndcg_score(true_relevance, np.asarray([rec_scores]), k=10)
-    print("Ground Truth  "+ str(true_scores))
-    print("Youtube Ranked  "+ str(you_scores))
-    print("NDCG " + str(youtube_NDCG))
+    youtube_NDCG = ndcg_score(true_relevance, np.asarray([you_scores]), k=5)
+    recommenter_NDCG = ndcg_score(true_relevance, np.asarray([rec_scores]), k=5)
+    with open("NDCG2.txt", "w") as result:
+        print(f"Ground Truth Videos  " + str(true_vids), file=result)
+        print(f"Youtube Videos  " + str(you_vids), file=result)
+        print(f"Recommenter Videos  " + str(rec_vids), file=result)
+        print(f"Ground Truth  "+ str(true_scores), file=result)
+        print(f"Youtube Ranked  "+ str(you_scores), file=result)
+        print(f"Recommenter Ranked  " + str(rec_scores), file=result)
 
-    print("Youtube Ranked  " + str(rec_scores))
-    print("NDCG " + str(recommenter_NDCG))
+        print(f"YouTube NDCG " + str(youtube_NDCG), file=result)
+        print(f"Recommenter NDCG " + str(recommenter_NDCG), file=result)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if __name__ == "__main__":
     true_total, rec_total, you_total = pull_excelRanks()
     sorted_true = sorted(true_total, key=lambda x: x[0], reverse= True)
-    true_scores = [list(s) for s in zip(*sorted_true)][0][:10]
-    true_Vids = [list(s) for s in zip(*sorted_true)][1][:10]
+    true_scores = [list(s) for s in zip(*sorted_true)][0][:5]
+    true_Vids = [list(s) for s in zip(*sorted_true)][1][:5]
 
     you_scores = []
+    you_vids = []
     for score,vid in you_total:
+        you_vids.append(vid)
         if vid not in true_Vids:
             score = 0
         you_scores.append(score)
 
     rec_scores = []
+    rec_vids = []
     for score, vid in rec_total:
-
+        rec_vids.append(vid)
         if vid not in true_Vids:
             score = 0
         rec_scores.append(score)
 
-    calc_NDCG(true_scores, rec_scores, you_scores)
+    calc_NDCG(true_Vids, you_vids, rec_vids, true_scores, rec_scores, you_scores)
 
 
